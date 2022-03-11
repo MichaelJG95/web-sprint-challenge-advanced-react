d47d1cf5bfa037a43c50ca54b8d4083f2b51767c
Michael Gutierrez <michaelgutierrezj@gmail.com>
2022-03-11 17:11:12 -0600

    commit for mvp 1

--- frontend/components/AppClass.js
+++ frontend/components/AppClass.js
@@ -1,15 +1,85 @@
 import React from 'react'
+import axios from 'axios'
+
+const initialState = {
+  squares: [0,1,2,
+            3,4,5,
+            6,7,8],
+  activeSquare: 4,
+  steps: 0,
+  y: 2,
+  x: 2,
+  message: "",
+  input: "",
+}
 
 export default class AppClass extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = initialState
+  }
+
+  onChange = evt => {
+    const { value } = evt.target
+    this.setState({
+      ...this.state,
+      input: value,
+    })
+  }
+
+  submitQuery = (evt) => {
+    evt.preventDefault()
+    const query = { "x": this.state.x, "y": this.state.y, "steps": this.state.steps, "email": this.state.input }
+    axios.post("http://localhost:9000/api/result", query)
+      .then(res => {
+        this.setState({ ...this.state, message: res.data.message, input: "" })
+        // console.log(res.data.message)
+      })
+      .catch(err => {
+        this.setState({ ...this.state, message: err.response.data.message, input: "" })
+      })
+  }
+
+  keypadClickHandler = (event) => {
+    if(event.target.id === "reset") {
+      this.setState(initialState)
+    } else if(event.target.id === "left") {
+        if(this.state.x > 1){
+          this.setState({ ...this.state, steps: this.state.steps + 1, x: this.state.x - 1, activeSquare: this.state.activeSquare - 1, message: "" })
+        } else {
+          this.setState({ ...this.state, message: "You can't go left"})
+        }
+    } else if(event.target.id === "right") {
+        if(this.state.x < 3){
+          this.setState({ ...this.state, steps: this.state.steps + 1, x: this.state.x + 1, activeSquare: this.state.activeSquare + 1, message: "" })
+        } else {
+          this.setState({ ...this.state, message: "You can't go right"})
+        }
+    } else if(event.target.id === "up") {
+        if(this.state.y > 1){
+          this.setState({ ...this.state, steps: this.state.steps + 1, y: this.state.y - 1, activeSquare: this.state.activeSquare - 3, message: "" })
+        } else {
+          this.setState({ ...this.state, message: "You can't go up"})
+        }
+    } else if(event.target.id === "down") {
+        if(this.state.y < 3){
+          this.setState({ ...this.state, steps: this.state.steps + 1, y: this.state.y + 1, activeSquare: this.state.activeSquare + 3, message: "" })
+        } else {
+          this.setState({ ...this.state, message: "You can't go down"})
+        }
+    }
+  }
+
+
   render() {
     const { className } = this.props
     return (
       <div id="wrapper" className={className}>
         <div className="info">
-          <h3 id="coordinates">Coordinates (2, 2)</h3>
-          <h3 id="steps">You moved 0 times</h3>
+          <h3 id="coordinates">{`Coordinates (${this.state.x}, ${this.state.y})`}</h3>
+          <h3 id="steps">{`You moved ${this.state.steps} time${this.state.steps === 1 ? "" : "s"}`}</h3>
         </div>
-        <div id="grid">
+        {/* <div id="grid">
           <div className="square"></div>
           <div className="square"></div>
           <div className="square"></div>
@@ -19,22 +89,40 @@ export default class AppClass extends React.Component {
           <div className="square"></div>
           <div className="square"></div>
           <div className="square"></div>
-        </div>
+        </div> */}
+         <Grid squares={this.state.squares} activeSquare={this.state.activeSquare} />
         <div className="info">
-          <h3 id="message"></h3>
+          <h3 id="message">{this.state.message}</h3>
         </div>
         <div id="keypad">
-          <button id="left">LEFT</button>
-          <button id="up">UP</button>
-          <button id="right">RIGHT</button>
-          <button id="down">DOWN</button>
-          <button id="reset">reset</button>
+          <button id="left" onClick={this.keypadClickHandler}>LEFT</button>
+          <button id="up" onClick={this.keypadClickHandler}>UP</button>
+          <button id="right" onClick={this.keypadClickHandler}>RIGHT</button>
+          <button id="down" onClick={this.keypadClickHandler}>DOWN</button>
+          <button id="reset" onClick={this.keypadClickHandler}>reset</button>
         </div>
-        <form>
-          <input id="email" type="email" placeholder="type email"></input>
+        <form onSubmit={this.submitQuery}>
+          <input 
+            onChange={this.onChange}
+            value={this.state.input}
+            id="email" 
+            type="email" 
+            placeholder="type email"></input>
           <input id="submit" type="submit"></input>
         </form>
       </div>
     )
   }
+}
+
+class Grid extends React.Component {
+  render(){
+  return(
+    <div id="grid">
+      {this.props.squares.map(square => {
+        return <div className={square === this.props.activeSquare ? "square active" : "square"} >{square === this.props.activeSquare ? "B" : ""}</div>
+      })}
+    </div>
+  )
+}
 }
--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,37 +1,136 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const initialState = {
+  squares: [0,1,2,
+            3,4,5,
+            6,7,8],
+  activeSquare: 4,
+  steps: 0,
+  y: 2,
+  x: 2,
+  input: "",
+}
+
+
 
 export default function AppFunctional(props) {
+
+  const [appState, setAppState] = useState(initialState)
+  const [message, setMessageState] = useState("")
+  const y = appState.y
+  const x = appState.x
+  const steps = appState.steps
+
+  const onChange = evt => {
+    const { value } = evt.target
+    setAppState({
+      ...appState,
+      input: value,
+    })
+  }
+
+  const submitQuery = (evt) => {
+    evt.preventDefault()
+    const query = { "x": appState.x, "y": appState.y, "steps": appState.steps, "email": appState.input }
+    axios.post("http://localhost:9000/api/result", query)
+      .then(res => {
+        setMessageState(res.data.message)
+        setAppState({ ...appState, input: "" })
+        // console.log(res.data.message)
+      })
+      .catch(err => {
+        setMessageState(err.response.data.message)
+        setAppState({ ...appState, input: "" })
+        // setMessageState(err.message)
+        // console.error(err)
+      })
+  }
+
+  const keypadClickHandler = (event) => {
+    if(event.target.id === "reset") {
+      setAppState(initialState)
+      setMessageState("")
+    } else if(event.target.id === "left") {
+        if(appState.x > 1){
+          setAppState({ ...appState, steps: appState.steps + 1, x: appState.x - 1, activeSquare: appState.activeSquare - 1 })
+          setMessageState("")
+        } else {
+          setMessageState("You can't go left")
+        }
+    } else if(event.target.id === "right") {
+        if(appState.x < 3){
+          setAppState({ ...appState, steps: appState.steps + 1, x: appState.x + 1, activeSquare: appState.activeSquare + 1 })
+          setMessageState("")
+        } else {
+          setMessageState("You can't go right")
+        }
+    } else if(event.target.id === "up") {
+        if(appState.y > 1){
+          setAppState({ ...appState, steps: appState.steps + 1, y: appState.y - 1, activeSquare: appState.activeSquare - 3 })
+          setMessageState("")
+        } else {
+          setMessageState("You can't go up")
+        }
+    } else if(event.target.id === "down") {
+        if(appState.y < 3){
+          setAppState({ ...appState, steps: appState.steps + 1, y: appState.y + 1, activeSquare: appState.activeSquare + 3 })
+          setMessageState("")
+        } else {
+          setMessageState("You can't go down")
+        }
+    }
+  }
+
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Coordinates (2, 2)</h3>
-        <h3 id="steps">You moved 0 times</h3>
+        <h3 id="coordinates">{`Coordinates (${x}, ${y})`}</h3>
+        <h3 id="steps">{`You moved ${steps} time${steps === 1 ? "" : "s"}`}</h3>
       </div>
-      <div id="grid">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square active">B</div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-      </div>
+      <Grid squares={appState.squares} activeSquare={appState.activeSquare} />
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">LEFT</button>
-        <button id="up">UP</button>
-        <button id="right">RIGHT</button>
-        <button id="down">DOWN</button>
-        <button id="reset">reset</button>
+        <button id="left" onClick={keypadClickHandler}>LEFT</button>
+        <button id="up" onClick={keypadClickHandler}>UP</button>
+        <button id="right" onClick={keypadClickHandler}>RIGHT</button>
+        <button id="down" onClick={keypadClickHandler}>DOWN</button>
+        <button id="reset" onClick={keypadClickHandler}>reset</button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="type email"></input>
-        <input id="submit" type="submit"></input>
+      <form onSubmit={submitQuery}>
+        <input
+          onChange={onChange} 
+          value={appState.input}
+          id="email" 
+          type="email" 
+          placeholder="type email"
+       
+        />
+        <input 
+          id="submit" 
+          type="submit"
+        />
       </form>
     </div>
   )
 }
+
+function Grid(props) {
+  return(
+    <div id="grid">
+      {props.squares.map(square => {
+        return <div className={square === props.activeSquare ? "square active" : "square"} >{square === props.activeSquare ? "B" : ""}</div>
+      })}
+    </div>
+  )
+}
+
+
+
+// function Square(props) {
+//   return(
+//     <div className="square"></div>
+//   )
+// }
